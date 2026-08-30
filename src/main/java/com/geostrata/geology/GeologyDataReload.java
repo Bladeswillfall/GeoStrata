07ad185feca0e2485cac37da3fc52007c7829c17
Fall <81432164+Bladeswillfall@users.noreply.github.com>
2026-08-30 22:02:40 +0100

    Drop obsolete single-background validation

--- src/main/java/com/geostrata/geology/GeologyDataReload.java
+++ src/main/java/com/geostrata/geology/GeologyDataReload.java
@@ -88,9 +88,6 @@ public final class GeologyDataReload {
             boolean companionLoaded
     ) {
         LithologyCatalog.Snapshot lithologies = LithologyCatalog.parse(lithologiesRoot);
-        for (GeologyProvince province : GeologyProvince.values()) {
-            lithologies.require(province.backgroundLithology());
-        }
         OreOccurrenceCatalog.Snapshot oreOccurrences = OreOccurrenceCatalog.parse(
                 lithologies,
                 oreOccurrencesRoot
