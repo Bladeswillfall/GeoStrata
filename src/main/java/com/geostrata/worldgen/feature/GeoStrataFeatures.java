72cea4462a987ea9d1879cf596ec001b94d2559e
Fall <81432164+Bladeswillfall@users.noreply.github.com>
2026-08-30 21:34:11 +0100

    Register province background feature definition

--- src/main/java/com/geostrata/worldgen/feature/GeoStrataFeatures.java
+++ src/main/java/com/geostrata/worldgen/feature/GeoStrataFeatures.java
@@ -7,6 +7,7 @@ import net.minecraft.world.gen.feature.Feature;
 public final class GeoStrataFeatures {
     public static final Feature<StrataLensConfig> STRATA_LENS = new StrataLensFeature(StrataLensConfig.CODEC);
     public static final Feature<DefaultFeatureConfig> CORRELATED_SEDIMENTARY = new CorrelatedSedimentaryFeature();
+    public static final Feature<DefaultFeatureConfig> PROVINCE_BACKGROUND = new ProvinceBackgroundFeature();
     public static final Feature<DefaultFeatureConfig> ORE_DEPOSIT = new OreDepositFeature();
     public static final Feature<DiamondPipeConfig> DIAMOND_PIPE = new DiamondPipeFeature(DiamondPipeConfig.CODEC);
     public static final Feature<DefaultFeatureConfig> DIAMOND_STRUCTURAL = new DiamondStructuralFeature();
